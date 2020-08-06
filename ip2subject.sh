31fc06eb4d9871996a8d34022e61b81a6d5fc3fb
Frank Breedijk <fbreedijk@schubergphilis.com>
2020-08-06 13:48:03 +0200

    More rebustness

--- ip2subject.sh
+++ ip2subject.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+
 if [[ "$1" == "-h" || "$1" == "--help" || -z $1 ]]; then
 	echo "$0 [-h|--help] HOST [PORT]"
 	echo "This script connects to HOST on PORT (443 by default) and gets the subject from the certificate."
-	exit 0
 fi
 
 if [[ -z $2 ]]; then
@@ -11,15 +11,16 @@ else
 	port=$2
 fi
 
-set +e
 gtimeout 1 bash -c "cat < /dev/null > /dev/tcp/$1/443"
 if [[ "$?" -eq 0 ]]; then
-	set -e
-	cert=`echo ""|openssl s_client -showcerts -servername divd.nl -connect $1:443 2>/dev/null`
-	parsed=`echo "$cert" | openssl x509 -inform pem -noout -text`
-	subject=`echo "$parsed"|grep Subject:|sed 's/^.*CN\=//'|sed 's/\/emailAddress.*//'`
+	cert=`gtimeout 5 bash -c "echo ""|openssl s_client -showcerts -servername divd.nl -connect $1:443 2>/dev/null"`
+	if [[ ! -z "$cert" ]]; then
+		parsed=`echo "$cert" | openssl x509 -inform pem -noout -text`
+		subject=`echo "$parsed"|grep Subject:|sed 's/^.*CN\=//'|sed 's/\/emailAddress.*//'`
+	else
+		subject="*no tls*"
+	fi
 	echo "$1	$subject"
 else
-	set -e
 	echo "$1	*down*"
 fi
